5874f9b0af186c5b2bd7c805309d4c36e3b7dccb
DannySuarez <suarezd10@gmail.com>
2019-07-10 17:05:42 -0700

    logic for routes

--- http/lib/app.js
+++ http/lib/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 
+const dogs = [
+  { name: 'spot', age: 5, weight: '20lbs' },
+  { name: 'rover', age: 3, weight: '15lbs' },
+  { name: 'scooter', age: 6, weight: '10lbs' },
+];
 
-app.use(express.static('./public'));
-// app.get('./api/v1/dog', (req, res) => {
-//   res.send({ name: 'spot', age: 5, weight: '20lbs' });
-// });
+app.use(express.static('./http/public'));
+
+app.get('/dogs', (req, res) => {
+  res.send(dogs);
+});
 
 module.exports = app;
